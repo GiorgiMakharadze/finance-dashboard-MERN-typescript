c46ae571075bd619a1bbbcdd861075b39a056f4c
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-05-09 22:04:45 +0400

    connected to mongodb

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import "express-async-errors";
 import helmet from "helmet";
 import morgan from "morgan";
 import xss from "xss-clean";
+import { connectDB } from "./db/connect";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
+
+//middleware & security
 
 app.use(express.json());
 app.use(helmet());
@@ -15,6 +19,15 @@ app.use(morgan("common"));
 app.use(xss());
 app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Server is listening ong port: ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL!);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port} ...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ server/types/mongoose.d.ts
@@ -0,0 +1,5 @@
+declare module "mongoose" {
+  const value: any;
+
+  export default value;
+}
